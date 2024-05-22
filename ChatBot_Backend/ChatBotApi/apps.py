dd7e0cf09cb9678e5279894f0e62ed6b5f9663fb
Yogesh Singh <116149692+yogeshsingh-11@users.noreply.github.com>
2024-05-22 12:07:51 +0530

    Delete ChatBot_Backend/ChatBotApi directory

--- ChatBot_Backend/ChatBotApi/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChatbotapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ChatBotApi'
--- ChatBot_Backend/ChatBotApi/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-query_engine = index.as_query_engine()
-    response = query_engine.query(query)
